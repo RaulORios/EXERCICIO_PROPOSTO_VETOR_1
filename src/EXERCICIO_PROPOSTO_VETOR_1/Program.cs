4cc858ac6b5e587a2def9c3eccf925e7f110c112
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:25:13 -0300

    Program.cs adicionado

---
+++ src/EXERCICIO_PROPOSTO_VETOR_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace EXERCICIO_PROPOSTO_VETOR_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            double[] valores = new double[n];
+
+            string[] vet = Console.ReadLine().Split(' ');
+            double maior = 0; 
+            int posicao = 0;
+            for (int i = 0; i < n; i++)
+            {                
+                valores[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+                if (valores[i] > maior)
+                {
+                    maior = valores[i];
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(posicao);
+
+            Console.ReadKey();
+
+        }
+    }
+}
